c1ef89f0f6fa2d8175076c5dd41a86fee64dc571
rsgc-leder-b <bleder@rsgc.on.ca>
2016-10-27 14:02:58 -0400

    Finished part 1-Build a Basic UI. Setup a button, label and textfield. Enables constraints by using stacks and normal position constraints.

---
+++ FoodTracker/FoodPlayground.playground/Contents.swift
@@ -0,0 +1,23 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+enum Suit {
+    case Spades, Hearts, Diamonds, Clubs
+    func simpleDescription() -> String {
+        switch self {
+        case .Spades:
+            return "spades"
+        case .Hearts:
+            return "hearts"
+        case .Diamonds:
+            return "diamonds"
+        case .Clubs:
+            return "clubs"
+        }
+    }
+}
+let hearts = Suit.Hearts
+let heartsDescription = hearts.simpleDescription()
