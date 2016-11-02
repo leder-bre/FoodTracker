512105805f18fadbbe5f42b6172fc714ff8f3737
rsgc-leder-b <bleder@rsgc.on.ca>
2016-11-02 14:15:19 -0400

    Added a ratingControl file and a corresponding view object in the storyboard.

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,17 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Brendan Leder on 2016-11-02.
+//  Copyright © 2016 Brendan Leder. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIView {
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+}
