9e96cecf102abc9300db3a511da2fbcac23af569
rsgc-leder-b <bleder@rsgc.on.ca>
2016-11-01 20:02:15 -0400

    For this update I finished the segment "Working with View Controllers". In this section I began by adding an image view to the app and using the attributes editor to make it so that the UIImageView could resize from device to device. After that I learned how to add an external image to Xcode using the assets folder. After that I added something called a 'gesture recognizer,' which allowed non-interactive objects (Images, labels) to detect gestures (a touch in this case) and call upon an action in code. Next I set up the ViewController class so that it supported a photo picker, by making it an enumeration of UINavigationControllerDelagate, which allowed me to add functions like imagePickerController so that the user, when clicking on the image, could pick a photo to add to their recipe.

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,10 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     //MARK: Properties
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,5 +32,23 @@ class ViewController: UIViewController, UITextFieldDelegate {
     //MARK: Actions
     @IBAction func setDefaultLabelText(_ sender: UIButton) {
         mealNameLabel.text = "Default Text"
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let selectedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        photoImageView.image = selectedImage
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func selectImageFromPhotoLibrary(_ sender: AnyObject) {
+        nameTextField.resignFirstResponder()
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
     }
 }
