87d8fb604e9587e0812c0122b589c9af82349f8c
rsgc-leder-b <bleder@rsgc.on.ca>
2016-11-04 08:56:59 -0400

    Added code (what a surprise) to add variables for spacing the the number of buttons, and used those variables to space out the buttons, allowing the user to view all 5 (or whatever starCount is) buttons.

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -12,10 +12,19 @@ class RatingControl: UIView {
     // MARK: Properties
     var rating = 0
     var ratingButtons = [UIButton]()
-
+    let spacing = 5
+    let starCount = 5
+    // MARK: Initialization
+    override func layoutSubviews() {
+        var buttonFrame = CGRect(x: 0, y: 0, width: 44, height: 44)
+        for(index,button) in ratingButtons.enumerated() {
+            buttonFrame.origin.x = CGFloat(index * (44 + spacing))
+            button.frame = buttonFrame
+        }
+    }
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        for i in 0...4 {
+        for _ in 0...(starCount-1) {
             let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
             button.backgroundColor = UIColor.red
             button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchDown)
