03a8c91017fd8d426bdcf03dfd59b1142e1c1b0b
rsgc-leder-b <bleder@rsgc.on.ca>
2016-11-05 18:46:45 -0400

    Finished the RatingControl file and attached the corresponding object on the storyboard to the main code in ViewController. Removed the SetDefaultText button and its action in code. Now starting 'Define Your Data Model'

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
 	@IBOutlet weak var nameTextField: UITextField!
 	@IBOutlet weak var mealNameLabel: UILabel!
 	@IBOutlet weak var photoImageView: UIImageView!
+	@IBOutlet weak var ratingControl: RatingControl!
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -47,8 +48,5 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
 		imagePickerController.sourceType = .photoLibrary
 		imagePickerController.delegate = self
 		present(imagePickerController, animated: true, completion: nil)
-	}
-	@IBAction func setDefaultLabelText(_ sender: UIButton) {
-		mealNameLabel.text = "Default Text"
 	}
 }
