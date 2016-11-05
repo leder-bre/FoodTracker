1f760c743690d0af9d40693d59a4cadd544c93d5
rsgc-leder-b <bleder@rsgc.on.ca>
2016-11-05 19:23:46 -0400

    Learned how to use a 'test' file to see if individual classes should return nil or not nil under specific test circumstances and tested our Meal class using all three parameters.

--- FoodTrackerTests/FoodTrackerTests.swift
+++ FoodTrackerTests/FoodTrackerTests.swift
@@ -10,27 +10,14 @@ import XCTest
 @testable import FoodTracker
 
 class FoodTrackerTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    //MARK: FoodTracker Tests
+    //Test to see if Meal initalizer returns properly
+    func testMealInitialization() {
+        let potentialItem = Meal(name:"Test Meal", photo: nil, rating: 5)
+        XCTAssertNotNil(potentialItem)
+        let noName = Meal(name:"", photo: nil, rating: 5)
+        XCTAssertNil(noName, "EmptyNameInvalid")
+        let worseThanXCode = Meal(name: "Tee-Rash", photo: nil, rating: -1)
+        XCTAssertNotNil(worseThanXCode)
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
