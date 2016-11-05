53dcee89043f0686b5fc1bc5c30c1b64219e8094
rsgc-leder-b <bleder@rsgc.on.ca>
2016-11-05 18:23:37 -0400

    Finished loading in star images into the application using the assets folder and implemented them into the rating control view. Added code that switches the image of the button based on whether or not its highlighted and selected. Next will add code that allows the app to tell when a button is highlighted or selected.

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -9,36 +9,47 @@
 import UIKit
 
 class RatingControl: UIView {
-    // MARK: Properties
-    var rating = 0
-    var ratingButtons = [UIButton]()
-    let spacing = 5
-    let starCount = 5
-    // MARK: Initialization
-    override func layoutSubviews() {
-        var buttonFrame = CGRect(x: 0, y: 0, width: 44, height: 44)
-        for(index,button) in ratingButtons.enumerated() {
-            buttonFrame.origin.x = CGFloat(index * (44 + spacing))
-            button.frame = buttonFrame
-        }
-    }
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        for _ in 0...(starCount-1) {
-            let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
-            button.backgroundColor = UIColor.red
-            button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchDown)
-            ratingButtons += [button]
-            addSubview(button)
-        }
-    }
-    override public var intrinsicContentSize: CGSize {
-        get {
-            return CGSize(width: 240, height: 44)
-        }
-    }
-    // MARK: Button Action
-    func ratingButtonTapped(button: UIButton) {
-        print("Button Pressed")
-    }
+	// MARK: Properties
+	var rating = 0
+	var ratingButtons = [UIButton]()
+	let spacing = 5
+	let starCount = 5
+	// MARK: Initialization
+	required init?(coder aDecoder: NSCoder) {
+		super.init(coder: aDecoder)
+		let filledStarImage = #imageLiteral(resourceName: "filledStar")
+		let emptyStarImage = #imageLiteral(resourceName: "emptyStar")
+		for _ in 0...(starCount-1) {
+			let button = UIButton()
+			button.setImage(emptyStarImage, for: .normal)
+			button.setImage(filledStarImage, for: .selected)
+			button.setImage(filledStarImage, for: [.highlighted, .selected])
+			//button.backgroundColor = UIColor.red
+			button.adjustsImageWhenHighlighted = false
+			button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchDown)
+			ratingButtons += [button]
+			addSubview(button)
+		}
+	}
+	override func layoutSubviews() {
+		let buttonSize = Int(frame.size.height)
+		print("height2: \(buttonSize)")
+		var buttonFrame = CGRect(x: 0, y: 0, width: buttonSize, height: buttonSize)
+		for(index,button) in ratingButtons.enumerated() {
+			buttonFrame.origin.x = CGFloat(index * (44 + spacing))
+			button.frame = buttonFrame
+		}
+	}
+	override public var intrinsicContentSize: CGSize {
+		get {
+			let buttonSize = Int(frame.size.height)
+			let width = (buttonSize * starCount) + (spacing * (starCount-1))
+			print("height: \(buttonSize)")
+			return CGSize(width: width, height: 44) //ERROR: buttonSize is always 1000 despite being 44 in layoutSubviews, meaning this is called before frame setup
+		}
+	}
+	// MARK: Button Action
+	func ratingButtonTapped(button: UIButton) {
+		print("Button Pressed")
+	}
 }
--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -9,46 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    //MARK: Properties
-    @IBOutlet weak var nameTextField: UITextField!
-    @IBOutlet weak var mealNameLabel: UILabel!
-    @IBOutlet weak var photoImageView: UIImageView!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        //Handle the text field's user input through delegate calls
-        nameTextField.delegate = self
-        //Set the view controller to the delegating object
-        
-    }
-    //MARK: UITextFieldDelegate
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
-        return true
-    }
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        mealNameLabel.text = textField.text
-    }
-    //MARK: UIImagePickerDelegate
-    
-    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        dismiss(animated: true, completion: nil)
-    }
-    
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-        let selectedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
-        photoImageView.image = selectedImage
-        dismiss(animated: true, completion: nil)
-    }
-    //MARK: Actions
-    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
-        nameTextField.resignFirstResponder()
-        let imagePickerController = UIImagePickerController()
-        imagePickerController.sourceType = .photoLibrary
-        imagePickerController.delegate = self
-        present(imagePickerController, animated: true, completion: nil)
-    }
-    @IBAction func setDefaultLabelText(_ sender: UIButton) {
-        mealNameLabel.text = "Default Text"
-    }
+	//MARK: Properties
+	@IBOutlet weak var nameTextField: UITextField!
+	@IBOutlet weak var mealNameLabel: UILabel!
+	@IBOutlet weak var photoImageView: UIImageView!
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		//Handle the text field's user input through delegate calls
+		nameTextField.delegate = self
+		//Set the view controller to the delegating object
+		
+	}
+	//MARK: UITextFieldDelegate
+	func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+		textField.resignFirstResponder()
+		return true
+	}
+	func textFieldDidEndEditing(_ textField: UITextField) {
+		mealNameLabel.text = textField.text
+	}
+	//MARK: UIImagePickerDelegate
+	
+	func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+		dismiss(animated: true, completion: nil)
+	}
+	
+	func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+		let selectedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+		photoImageView.image = selectedImage
+		dismiss(animated: true, completion: nil)
+	}
+	//MARK: Actions
+	@IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+		nameTextField.resignFirstResponder()
+		let imagePickerController = UIImagePickerController()
+		imagePickerController.sourceType = .photoLibrary
+		imagePickerController.delegate = self
+		present(imagePickerController, animated: true, completion: nil)
+	}
+	@IBAction func setDefaultLabelText(_ sender: UIButton) {
+		mealNameLabel.text = "Default Text"
+	}
 }
