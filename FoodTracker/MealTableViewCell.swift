179441fb8a327f1894a1324b2e240593526c4956
rsgc-leder-b <bleder@rsgc.on.ca>
2016-11-05 19:36:35 -0400

    Added a customized table cell to the table view that features an image, rating and label.

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Brendan Leder on 2016-11-05.
+//  Copyright © 2016 Brendan Leder. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
