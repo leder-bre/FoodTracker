d62a1cd6ac8ba6c4293c597ec2b932c35c15eac8
rsgc-leder-b <bleder@rsgc.on.ca>
2016-11-05 20:26:24 -0400

    Changed the name of ViewController to MealViewController and removed nameLabel for the lulz.

--- FoodTracker/MealViewController.swift
+++ FoodTracker/MealViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MealViewController.swift
 //  FoodTracker
 //
 //  Created by Brendan Leder on 2016-10-25.
@@ -8,10 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 	//MARK: Properties
 	@IBOutlet weak var nameTextField: UITextField!
-	@IBOutlet weak var mealNameLabel: UILabel!
 	@IBOutlet weak var photoImageView: UIImageView!
 	@IBOutlet weak var ratingControl: RatingControl!
 	
@@ -28,7 +27,6 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
 		return true
 	}
 	func textFieldDidEndEditing(_ textField: UITextField) {
-		mealNameLabel.text = textField.text
 	}
 	//MARK: UIImagePickerDelegate
 	
